3cb5f380d6621e5b48328d88ed95f22dfe27c802
Idrios <idrios@naiatutorials.com>
2022-06-28 14:21:14 -0400

    initial commit

---
+++ app/src/main/java/dev/jamesroberts/timecube/MainActivity.kt
@@ -0,0 +1,14 @@
+package dev.jamesroberts.timecube
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Created by James Roberts 28-Jun-2022
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
