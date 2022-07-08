c3be3bece349bd00a36dce9efd4ed0c8d12a1d0e
Idrios <idrios@naiatutorials.com>
2022-07-08 10:57:49 -0400

    add placeholder image for where recyclerview will go while fleshing out details of TimeView

--- app/src/main/java/dev/jamesroberts/timecube/ui/view/TimePicker.kt
+++ app/src/main/java/dev/jamesroberts/timecube/ui/view/TimePicker.kt
@@ -1,8 +1,15 @@
 package dev.jamesroberts.timecube.ui.view
 
 import android.content.Context
+import android.os.Bundle
 import android.util.AttributeSet
+import android.view.ContextMenu
+import android.view.LayoutInflater
 import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.ScrollView
+import androidx.recyclerview.widget.RecyclerView
+import dev.jamesroberts.timecube.R
 
 class TimePicker : FrameLayout {
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) :
@@ -13,4 +20,11 @@ class TimePicker : FrameLayout {
             super(context, attrs){}
     constructor(context: Context) :
             super(context){}
+
+    init{
+        inflate(context, R.layout.view_time_picker, this);
+    }
+
+    private var _currentTime : String = "0"
+
 }
--- app/src/main/java/dev/jamesroberts/timecube/ui/view/TimeView.kt
+++ app/src/main/java/dev/jamesroberts/timecube/ui/view/TimeView.kt
@@ -2,6 +2,7 @@ package dev.jamesroberts.timecube.ui.view
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import androidx.appcompat.widget.LinearLayoutCompat
 import dev.jamesroberts.timecube.R
 
@@ -10,12 +11,11 @@ class TimeView : LinearLayoutCompat {
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs, 0) {}
     constructor(context: Context) : super(context, null, 0) {}
 
-    override fun onFinishInflate() {
-        super.onFinishInflate()
-        _hourPicker = findViewById<TimePicker>(R.id.hourpicker)
-        _minutePicker = findViewById<TimePicker>(R.id.minutepicker)
-        _ampmPicker = findViewById<TimePicker>(R.id.ampmpicker)
-
+    init {
+        inflate(context, R.layout.view_time, this)
+        _hourPicker = findViewById<TimePicker>(R.id.hour_picker)
+        _minutePicker = findViewById<TimePicker>(R.id.minute_picker)
+        _ampmPicker = findViewById<TimePicker>(R.id.ampm_picker)
     }
 
     private var _hourPicker : TimePicker? = null;
